373c87d8bf792a707f9f8fb98bf42c5c5fbb0e4c
Lola Serrano <78372005+lolaserrano28@users.noreply.github.com>
2023-05-15 14:23:56 -0600

    codigos simulación difusiones

--- Códigos/Browniano Estandar.py
+++ Códigos/Browniano Estandar.py
@@ -21,15 +21,7 @@ plt.plot(MB(2)[0] ,MB(2)[1])
 plt.title("Movimiento Browniano Estándar")
 plt.show()
 
-def MBG(T, X0, mu, sigma, N = 1000):
-    mb = MB(T,N)[1]
-    T_ = np.linspace(0, T, N+1)
-    gbm = X0*np.exp((mu-sigma**2/2)*T_+sigma*mb)
-    return(T_,gbm)
-
-plt.plot(MBG(0.5,0.1,0,0.1,1000)[0] ,MBG(0.5,0.1,0,0.1,1000)[1])
-plt.title("Movimiento Browniano Geométrico")
-plt.show()
+#%%
 
 def MBG_milstein(T, X0, mu, sigma, N = 1000):
     X = [X0]
@@ -41,8 +33,43 @@ def MBG_milstein(T, X0, mu, sigma, N = 1000):
         X.append(Xi)
     return(T_,X)
 
-plt.plot(MBG_milstein(0.5,0.1,0,0.1,1000)[0] ,MBG_milstein(0.5,0.1,0,0.1,1000)[1])
+plt.plot(MBG_milstein(1,1,0,0.1,1000)[0] ,MBG_milstein(1,1,0,0.1,1000)[1], label = "$\mu=0$")
+plt.plot(MBG_milstein(1,1,0.25,0.1,1000)[0] ,MBG_milstein(1,1,0.25,0.1,1000)[1], label = "$\mu=0.25$")
+plt.plot(MBG_milstein(1,1,0.5,0.1,1000)[0] ,MBG_milstein(1,1,0.5,0.1,1000)[1], label = "$\mu=0.5$")
 plt.title("Movimiento Browniano Geométrico: Milstein")
+plt.legend()
 plt.show()
 
+plt.plot(MBG_milstein(1,1,0,0.1,1000)[0] ,MBG_milstein(1,1,0,0.1,1000)[1], label = "$\sigma=0.1$")
+plt.plot(MBG_milstein(1,1,0,0.5,1000)[0] ,MBG_milstein(1,1,0,0.5,1000)[1], label = "$\sigma=0.5$")
+plt.plot(MBG_milstein(1,1,0,1.2,1000)[0] ,MBG_milstein(1,1,0,1.2,1000)[1], label = "$\sigma=1.2$")
+plt.title("Movimiento Browniano Geométrico: Milstein")
+plt.legend()
+plt.show()
+
+#%%
+
+def MBG_Lamperti(T, X0, mu, sigma, N = 1000):
+    X = [X0]
+    Dt = T/1000
+    T_ = np.linspace(0, T, N+1)
+    for i in range(1,N+1):
+        Z = np.random.normal(0,1)
+        Ln_Xi = math.log(X[i-1]) + (mu-0.5*sigma**2)*Dt + sigma*np.sqrt(Dt)*Z
+        X.append(np.exp(Ln_Xi)) 
+    return(T_,X)
+
+plt.plot(MBG_Lamperti(1,1,0,0.1,1000)[0] ,MBG_Lamperti(1,1,0,0.1,1000)[1], label = "$\mu=0$")
+plt.plot(MBG_Lamperti(1,1,0.25,0.1,1000)[0] ,MBG_Lamperti(1,1,0.25,0.1,1000)[1], label = "$\mu=0.25$")
+plt.plot(MBG_Lamperti(1,1,0.5,0.1,1000)[0] ,MBG_Lamperti(1,1,0.5,0.1,1000)[1], label = "$\mu=0.5$")
+plt.title("Movimiento Browniano Geométrico: Lamperti")
+plt.legend()
+plt.show()
+
+plt.plot(MBG_Lamperti(1,1,0,0.1,1000)[0] ,MBG_Lamperti(1,1,0,0.1,1000)[1], label = "$\sigma=0.1$")
+plt.plot(MBG_Lamperti(1,1,0,0.5,1000)[0] ,MBG_Lamperti(1,1,0,0.5,1000)[1], label = "$\sigma=0.5$")
+plt.plot(MBG_Lamperti(1,1,0,1.2,1000)[0] ,MBG_Lamperti(1,1,0,1.2,1000)[1], label = "$\sigma=1.2$")
+plt.title("Movimiento Browniano Geométrico: Lamperti")
+plt.legend()
+plt.show()
         
---
+++ Códigos/Puente Browniano.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 18:41:57 2023
+
+@author: lolas
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+def puente_browniano(T,a,b,N=1000):
+    X = [a]
+    Dt = T/1000
+    T_ = np.linspace(0, T, N+1)
+    for i in range(1,N+1):
+        Z = np.random.normal(0,1)
+        Xi = X[i-1] + ((b-X[i-1])/(T-T_[i-1]))*Dt + np.sqrt(Dt)*Z 
+        X.append(Xi)
+    return(T_,X)
+
+T = 1
+a = 0
+b = 2
+N = 1000
+plt.plot(puente_browniano(T,a,b,N)[0],puente_browniano(T,a,b,N)[1])
+plt.plot(puente_browniano(T,a,b,N)[0],puente_browniano(T,a,b,N)[1])
+plt.plot(puente_browniano(T,a,b,N)[0],puente_browniano(T,a,b,N)[1])
+plt.plot(puente_browniano(T,a,b,N)[0],puente_browniano(T,a,b,N)[1])
+plt.axhline(y = a,xmin = 0, xmax = T, linestyle = "--", color = "#458f75")
+plt.axhline(y = b,xmin = 0, xmax = T, linestyle = "--", color = "#458f75")
+plt.title("Puente Browniano: Ecuación Diferencial")
+plt.show()
+
+#%%
+def MB(T, N = 1000):
+    l = [np.random.normal(0,T/N) for i in range(N)]
+    T_ = np.linspace(0, T, N+1)
+    mb = np.insert(np.cumsum(l),0,0)
+    return(T_,mb)
+
+def puente_browniano2(T,N=1000):
+    T_ = MB(T,N)[0]
+    mb = MB(T,N)[1]
+    PB = mb - (T_/T)*mb[-1]
+    return(T_,PB)
+
+plt.plot(puente_browniano2(T,N)[0],puente_browniano2(T,N)[1])
+plt.plot(puente_browniano2(T,N)[0],puente_browniano2(T,N)[1])
+plt.plot(puente_browniano2(T,N)[0],puente_browniano2(T,N)[1])
+plt.axhline(y = 0,xmin = 0, xmax = T, linestyle = "--", color = "#458f75")
+plt.title("Puente Browniano: Representación Adelantada")
